2912e49444d46ac7880deaebbe3bb94599833435
typhonrt <support@typhonrt.org>
2021-06-01 15:48:05 -0700

    update tests

--- test/src/programmatic.test.js
+++ test/src/programmatic.test.js
@@ -1,33 +1,24 @@
-import url        from 'url';
+import fancy               from 'fancy-test';
+import { assert, expect }  from 'chai';
+import oclif               from '@oclif/core';
 
-import { assert } from 'chai';
+import testcli             from '../../src/index.js';
 
-import oclif      from '@oclif/core';
-
-describe('Programmatic Test CLI', () =>
+/**
+ * This tests utilize the main / programmatic export of the module.
+ */
+describe('Programmatic (API):', () =>
 {
-   let captureLog = '', originalLog;
-
-   beforeEach(() =>
+   it('bad command (shows how to use chai-as-promised)', async () =>
    {
-      originalLog = console.log;
-      console.log = (log) => { originalLog(log); captureLog += log; };
+      await expect(testcli('bad')).to.be.rejectedWith(oclif.Errors.CLIError, 'command bad not found');
    });
 
-   afterEach(() =>
-   {
-      console.log = originalLog;
-      captureLog = '';
-   });
-
-   it('run command test', (done) =>
-   {
-      oclif.run(['test'], url.fileURLToPath(import.meta.url))
-         .then(() =>
-         {
-            assert(captureLog, 'ran init hookran test command');
-            done();
-         })
-         .catch(oclif.Errors.handle);
-   });
+   fancy.fancy
+      .stdout()
+      .do(async () => await testcli('test'))
+      .it('run command test', (output) =>
+      {
+         assert.strictEqual(output.stdout, 'ran init hook\nran test command\n');
+      });
 });
